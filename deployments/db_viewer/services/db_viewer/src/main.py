59326c373ebfef8fe7788218949047752126f865
Tanner Peterson <tannermp23@hotmail.com>
2022-02-18 15:25:40 -0800

    comment out db code to get route working first

--- deployments/db_viewer/services/db_viewer/src/main.py
+++ deployments/db_viewer/services/db_viewer/src/main.py
@@ -6,32 +6,32 @@ import os
 
 from .aws_utils import get_ssm_secrets
 
-DB_CLUSTER_ENDPOINT = os.environ.get("DB_CLUSTER_ENDPOINT")
+# DB_CLUSTER_ENDPOINT = os.environ.get("DB_CLUSTER_ENDPOINT")
 
-username, password = get_ssm_secrets()
-INFO_DICT = {
-    "db_username": username,
-    "db_password": password,
-    "db_name": "mantarray_units",
-}
+# username, password = get_ssm_secrets()
+# INFO_DICT = {
+#     "db_username": username,
+#     "db_password": password,
+#     "db_name": "mantarray_units",
+# }
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-def get_db_connection():
-    return pymysql.connect(
-        host=DB_CLUSTER_ENDPOINT,
-        user=INFO_DICT["db_username"],
-        passwd=INFO_DICT["db_password"],
-        db=INFO_DICT["db_name"],
-    )
+# def get_db_connection():
+#     return pymysql.connect(
+#         host=DB_CLUSTER_ENDPOINT,
+#         user=INFO_DICT["db_username"],
+#         passwd=INFO_DICT["db_password"],
+#         db=INFO_DICT["db_name"],
+#     )
 
 
 @app.get("/")
 async def home(request: Request):
-    db = get_db_connection()
-    units = "TODO"
+    # db = get_db_connection()
+    units = [{"serial_number": "1", "hw_version": "2.2.0"}, {"serial_number": "2", "hw_version": "2.2.2"}]
     return templates.TemplateResponse(
         "table.html",
         {"request": request, "units": units},
