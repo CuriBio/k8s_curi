d3927fd1ce1a21efd070dc8590b8d111ba0a2fe0
Tanner Peterson <tannermp23@hotmail.com>
2022-02-18 15:22:47 -0800

    added skeleton code

--- deployments/db_viewer/services/db_viewer/src/main.py
+++ deployments/db_viewer/services/db_viewer/src/main.py
@@ -1,9 +1,38 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+import pymysql
+import os
+
+from .aws_utils import get_ssm_secrets
+
+DB_CLUSTER_ENDPOINT = os.environ.get("DB_CLUSTER_ENDPOINT")
+
+username, password = get_ssm_secrets()
+INFO_DICT = {
+    "db_username": username,
+    "db_password": password,
+    "db_name": "mantarray_units",
+}
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+def get_db_connection():
+    return pymysql.connect(
+        host=DB_CLUSTER_ENDPOINT,
+        user=INFO_DICT["db_username"],
+        passwd=INFO_DICT["db_password"],
+        db=INFO_DICT["db_name"],
+    )
 
 
 @app.get("/")
-async def root():
-    return JSONResponse({"message": "test"})
+async def home(request: Request):
+    db = get_db_connection()
+    units = "TODO"
+    return templates.TemplateResponse(
+        "table.html",
+        {"request": request, "units": units},
+    )
