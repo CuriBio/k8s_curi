ca0071a5a3be8f11d714e49cd805c6322e95e38e
Jason Person <jrperson@gmail.com>
2022-02-25 16:38:06 -0800

    cluster outputs

--- cluster/output.tf
+++ cluster/output.tf
@@ -1,19 +1,14 @@
 output "sg_worker_group_mgmt_one" {
-  description = "worker_group_mgmt_one security group arn"
-  value = aws_security_group.worker_group_mgmt_one.arn
+  description = "worker_group_mgmt_one security group"
+  value = aws_security_group.worker_group_mgmt_one
 }
 
 output "sg_all_worker_mgmt" {
-  description = "all_worker_management security group arn"
-  value = aws_security_group.all_worker_mgmt.arn
+  description = "all_worker_management security group"
+  value = aws_security_group.all_worker_mgmt
 }
 
-output "vpc_id" {
-  description = "cluster vpc id"
-  value = module.vpc.vpc_id
-}
-
-output "vpc_private_subnet_ids" {
-  description = "private vpc subnet ids"
-  value = module.vpc.private_subnets
+output "cluster_vpc" {
+  description = "cluster vpc"
+  value = module.vpc
 }
