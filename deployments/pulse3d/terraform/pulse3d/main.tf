fdd36f918ad147209d85f15419eba7e8743f0ad7
Jason Person <jrperson@gmail.com>
2022-03-23 10:46:20 -0700

    initial pulse3d api, fixed alembic migrations, utils

---
+++ deployments/pulse3d/terraform/pulse3d/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "pulse3d_ecr_repo" {
+  name                 = "pulse3d"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
