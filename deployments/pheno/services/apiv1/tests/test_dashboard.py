d0d5d15d4b51cec0e4e01dff13681ffda85b0a08
luciipak <luciypak@gmail.com>
2022-03-23 11:35:20 -0700

    added dashboard tests

--- deployments/pheno/services/apiv1/tests/test_dashboard.py
+++ deployments/pheno/services/apiv1/tests/test_dashboard.py
@@ -7,16 +7,18 @@ from src.lib import *
 from src.lib.models import *
 
 
+def test_usage_route__returns_correct_query_count(client, mock_cursor):
+    mock_cursor.fetchrow.return_value = {
+        "trainings": 1,
+    }
+    client.get("/usage/1?month=2&year=2022")
+
+    assert mock_cursor.fetchrow.await_count == 2
+
 def test_usage_route__returns_correct_response_model(client, mock_cursor):
     mock_cursor.fetchrow.return_value = {
         "trainings": 1,
-        "experiments": 2,
-        "segmentations": 3,
-        "segtrainings": 4,
-        "total_processingtime": 5,
     }
+    client.get("/usage/1?month=2&year=2022")
 
-    response = client.get("/usage/1?month=2&year=2022")
-
-    mock_cursor.fetchrow.assert_awaited()
-
+    assert mock_cursor.fetchrow.await_count == 2
