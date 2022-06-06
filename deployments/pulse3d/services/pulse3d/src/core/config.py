e7974bf8cc02e6c8a4d001fe5e00ef31de80587a
luciipak <luciypak@gmail.com>
2022-06-06 16:12:57 -0700

    removed default values

--- deployments/pulse3d/services/pulse3d/src/core/config.py
+++ deployments/pulse3d/services/pulse3d/src/core/config.py
@@ -5,7 +5,7 @@ config = Config(".env")
 
 APP_NAME = config("APP_NAME", cast=str, default="CuriBio/Pulse3d")
 ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int, default=5)  # 5 minutes
-JWT_SECRET_KEY = config("JWT_SECRET_KEY", cast=Secret, default="testjwt123")
+JWT_SECRET_KEY = config("JWT_SECRET_KEY", cast=Secret)
 JWT_ALGORITHM = config("JWT_ALGORITHM", cast=str, default="HS256")
 JWT_AUDIENCE = config("JWT_AUDIENCE", cast=str, default="curibio:auth")
 JWT_TOKEN_PREFIX = config("JWT_TOKEN_PREFIX", cast=str, default="Bearer")
