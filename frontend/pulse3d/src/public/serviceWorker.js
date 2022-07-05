1649629eaf1be4d19f50403fa7a47685f4c148f8
luciipak <luciypak@gmail.com>
2022-07-05 10:47:17 -0700

    updated service worker with correct domain

--- frontend/pulse3d/src/public/serviceWorker.js
+++ frontend/pulse3d/src/public/serviceWorker.js
@@ -2,11 +2,8 @@
 const domain = "curibio-test"; // MODIFY URL until decided how it's handled
 
 const getUrl = (endpoint) => {
-  // let subdomain = endpoint.includes("users") ? "apiv2" : "pulse3d";
-  let subdomain =
-    endpoint.includes("login") || endpoint.includes("logout") ? "8001" : "8000";
-  return new URL(`http://localhost:${subdomain}${endpoint}`);
-  // return new URL(`https://${subdomain}.${domain}.com${endpoint}`);
+  let subdomain = endpoint.includes("users") ? "apiv2" : "pulse3d";
+  return new URL(`https://${subdomain}.${domain}.com${endpoint}`);
 };
 
 const tokens = {
