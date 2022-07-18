678ea578d3ce5205ad3ef1fb8535fde487fc19f5
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2022-07-18 12:18:00 -0700

    Ci db migrations (#130)
    * added db-migrations-dry-run on PR open and db-migrations on PR merge

--- bin/curi_builder/builder/__init__.py
+++ bin/curi_builder/builder/__init__.py
@@ -8,6 +8,9 @@ import subprocess
 import requests
 
 
+K8S_REPO_BASE_URL = "https://api.github.com/repos/CuriBio/k8s_curi"
+
+
 def find_changed():
     r = subprocess.run(["git", "--no-pager", "diff", "main", "--name-only", "./deployments"], stdout=subprocess.PIPE)
     s = r.stdout.decode("utf-8").split("\n")[:-1]
@@ -24,14 +27,22 @@ def set_build_status(build, status, sha, token):
             "Content-Type": "application/json",
         },
         "data": json.dumps({"state": status, "context": build}),
-        "url": f"https://api.github.com/repos/curibio/k8s_curi/statuses/{sha}",
+        "url": f"{K8S_REPO_BASE_URL}/statuses/{sha}",
     }
 
     response = requests.post(**req)
-    if response.status_code == 201:
-        sys.exit(0)
-    else:
-        sys.exit(1)
+    return response.status_code != 201
+
+
+def post_pr_comment(pr_number, comment, token):
+    req = {
+        "headers": {"Authorization": f"Bearer {token.strip()}"},
+        "json": {"body": comment},
+        "url": f"{K8S_REPO_BASE_URL}/issues/{pr_number}/comments"
+    }
+
+    response = requests.post(**req)
+    return response.status_code != 201
 
 
 def main():
@@ -40,9 +51,13 @@ def main():
     parser.add_argument("--status", type=str, default=None)
     parser.add_argument("--context", type=str, default=None)
     parser.add_argument("--sha", type=str)
+    parser.add_argument("--pr-number", type=int, default=None)
+    parser.add_argument("--pr-comment", type=str, default=None)
     args = parser.parse_args()
 
     token = os.getenv("TOKEN")
+
+    task_failed = False
 
     if args.changed and args.sha and token:
         changed = find_changed()
@@ -50,8 +65,10 @@ def main():
 
         if args.status:
             for c in changed:
-                set_build_status(f"{c['deployment']}/{c['service']}", args.status, args.sha, token)
+                task_failed |= set_build_status(f"{c['deployment']}/{c['service']}", args.status, args.sha, token)
     elif args.status and args.context and token:
-        set_build_status(args.context, args.status, args.sha, token)
+        task_failed |= set_build_status(args.context, args.status, args.sha, token)
+    elif args.pr_number and args.pr_comment:
+        task_failed |= post_pr_comment(args.pr_number, args.pr_comment, token)
 
-    sys.exit(0)
+    sys.exit(int(task_failed))
