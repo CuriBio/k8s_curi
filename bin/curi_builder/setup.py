c77f2a0aaa465a1b19d20b18612ea635cedfa08e
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2022-07-19 13:53:28 -0700

    fixed flag (#138)
    * fixed flag
    
    * remove debug lines

--- bin/curi_builder/setup.py
+++ bin/curi_builder/setup.py
@@ -4,7 +4,14 @@ setup(
     name="builder",
     version="0.1",
     packages=["builder"],
-    install_requires=["requests>=2.27.1", "alembic>=1.7.7"],
+    install_requires=[
+        "requests>=2.27.1",
+        "alembic>=1.7.7",
+        "SQLAlchemy>=1.4.35",
+        "psycopg2-binary>=2.9.3",
+        "argon2-cffi>=21.3.0",
+        "argon2-cffi-bindings>=21.2.0",
+    ],
     zip_safe=False,
     entry_points={
         "console_scripts": ["builder=builder:main"],
