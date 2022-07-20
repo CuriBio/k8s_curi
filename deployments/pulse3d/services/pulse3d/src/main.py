16e74888cbb53dc57cddfab6e963aa051e199520
luciipak <luciypak@gmail.com>
2022-07-20 11:17:12 -0500

    testing

--- deployments/pulse3d/services/pulse3d/src/main.py
+++ deployments/pulse3d/services/pulse3d/src/main.py
@@ -44,6 +44,7 @@ app.add_middleware(
     allow_origins=[
         "https://dashboard.curibio-test.com",
         "https://dashboard.curibio.com",
+        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
