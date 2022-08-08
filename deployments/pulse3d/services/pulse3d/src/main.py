b5093c22570f7adf5c4884cd6e51e941e1bd3fe7
nikitapoly <nikitapoly@comcast.net>
2022-08-08 16:58:58 -0700

    fixed typo

--- deployments/pulse3d/services/pulse3d/src/main.py
+++ deployments/pulse3d/services/pulse3d/src/main.py
@@ -47,7 +47,6 @@ app.add_middleware(
     allow_origins=[
         "https://dashboard.curibio-test.com",
         "https://dashboard.curibio.com",
-        "http://localhost:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
