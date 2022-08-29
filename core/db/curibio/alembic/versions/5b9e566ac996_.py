1f95b34883514853737c24ea9ee8b9bf5cfb3ccc
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2022-08-29 12:44:16 -0700

    Fix build push + test (#194)
    add list workflow permission + a test DB revision

---
+++ core/db/curibio/alembic/versions/5b9e566ac996_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5b9e566ac996
+Revises: 5b1cb92fa435
+Create Date: 2022-08-29 11:03:07.338427
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "5b9e566ac996"
+down_revision = "5b1cb92fa435"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table("test", sa.Column("test", sa.String(12), primary_key=True))
+
+
+def downgrade():
+    op.drop_table("test")
