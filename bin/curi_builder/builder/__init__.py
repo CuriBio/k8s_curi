01d557c87290f17467f99fcd8d6e87213a6e411d
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2022-09-06 15:07:44 -0700

    Tag docker image (#207)
    * refactor curi_builder; parse pulse3d version from requirements.txt and output it
    
    * parametrize version in docker tag

--- bin/curi_builder/builder/__init__.py
+++ bin/curi_builder/builder/__init__.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import glob
 import os
 import sys
 import subprocess
@@ -11,6 +10,15 @@ import requests
 K8S_REPO_BASE_URL = "https://api.github.com/repos/CuriBio/k8s_curi"
 
 
+def parse_py_dep_version(file_path: str, dep_name: str) -> str:
+    with open(file_path) as f:
+        for line in f.readlines():
+            if line.startswith(dep_name):
+                return line.split("==")[-1]
+
+    raise Exception(f"{dep_name} not found in {file_path}")
+
+
 def find_changed(sha: str):
     list_to_return = []
 
@@ -18,45 +26,55 @@ def find_changed(sha: str):
         completed_process = subprocess.run(
             ["git", "--no-pager", "diff", sha, "--name-only", "--", dir, ":!*.tf"], stdout=subprocess.PIPE
         )
-        changes_list = completed_process.stdout.decode("utf-8").split("\n")[:-1]
-        list_to_return += [
-            {
-                "path": f"./{'/'.join(ch.split('/')[:-2])}",
-                "deployment": ch.split("/")[1],
-                # Splits the path into an array and return the element right before the src folder.
-                # If its the /deployment pulse3d directory, then change the service name from pulse3d to pulse3d_api
-                # Else set service name to be the folder one above src
-                "service": "pulse3d_api"
-                if ch.split("/")[ch.split("/").index("src") - 1] == "pulse3d" and dir == "./deployments"
-                else ch.split("/")[ch.split("/").index("src") - 1],
-            }
-            for ch in changes_list
-        ]
+        changed_paths_list = completed_process.stdout.decode("utf-8").split("\n")[:-1]
+
+        for ch_path in changed_paths_list:
+            subdirs = ch_path.split("/")
+
+            # set service name to be the folder one above src
+            svc = subdirs[subdirs.index("src") - 1]
+            if svc == "pulse3d" and dir == "./deployments":
+                # if it's the /deployment pulse3d directory, then change the service name from pulse3d to pulse3d_api
+                svc = "pulse3d_api"
+
+            # need to output the pulse3d package version so it can be included in the name of the pulse3d-worker docker image
+            version = (
+                parse_py_dep_version(ch_path, "pulse3d")
+                if dir == "./jobs" and os.path.basename(ch_path) == "requirements.txt"
+                else "latest"
+            )
+
+            # TODO test all this in a PR
+
+            list_to_return.append(
+                {
+                    "path": f"./{'/'.join(subdirs[:-2])}",
+                    "deployment": subdirs[1],
+                    "svc": svc,
+                    "version": version,
+                }
+            )
 
     return list_to_return
-    # ds = glob.glob('./deployments/**/Dockerfile', recursive=True)
-    # return [{"path": f"./{'/'.join(d.split('/')[:-1])}", "deployment": d.split("/")[2], "service": d.split("/")[4]} for d in ds]
 
 
 def find_changed_tf(sha):
     # get diff for all directories containing changed tf excluding those found in /cluster and /core
     completed_process = subprocess.run(
-        ["git", "--no-pager", "diff", sha, "--name-only", "--", "*.tf", ":!cluster", ":!core"], stdout=subprocess.PIPE
+        ["git", "--no-pager", "diff", sha, "--name-only", "--", "*.tf", ":!cluster", ":!core"],
+        stdout=subprocess.PIPE,
     )
     changed_paths = completed_process.stdout.decode("utf-8").split("\n")[:-1]
     # get only the first terraform directory, remove files
-    tf_dir_paths = [path.split("/terraform")[0] + "/terraform" for path in changed_paths]
-    # return unique tf paths
-    return [{"path": path.split("/terraform")[0] + "/terraform"} for path in list(set(tf_dir_paths))]
+    tf_dirs = set("".join(path.partition("/terraform")[:2]) for path in changed_paths)
+
+    return [{"path": path} for path in tf_dirs]
 
 
-def set_status(build, status, sha, token):
+def set_status(context, status, sha, token):
     req = {
-        "headers": {
-            "Authorization": f"Bearer {token.strip()}",
-            "Content-Type": "application/json",
-        },
-        "data": json.dumps({"state": status, "context": build}),
+        "headers": {"Authorization": f"Bearer {token.strip()}", "Content-Type": "application/json"},
+        "data": json.dumps({"state": status, "context": context}),
         "url": f"{K8S_REPO_BASE_URL}/statuses/{sha}",
     }
 
@@ -87,21 +105,26 @@ def main():
     args = parser.parse_args()
 
     token = os.getenv("TOKEN")
+    if not token:
+        raise Exception("No token set")
 
     task_failed = False
 
-    if args.changed and args.sha and token:
+    if args.status and args.sha:
+        if args.changed:
+            changed = (find_changed_tf if args.terraform else find_changed)(args.sha)
 
-        changed = find_changed_tf(args.sha) if args.terraform else find_changed(args.sha)
-        print(json.dumps(changed))
+            # printing here in order to expose the names of the svcs or terraform files with changes
+            # to the process calling this python script. It is not a debug statement, don't delete it
+            print(json.dumps(changed))
 
-        if args.status and not args.terraform:
             for c in changed:
                 context = c["path"] if args.terraform else f"{c['deployment']}/{c['service']}"
                 task_failed |= set_status(context, args.status, args.sha, token)
 
-    elif args.status and args.context and token:
-        task_failed |= set_status(args.context, args.status, args.sha, token)
+        if args.context:
+            task_failed |= set_status(args.context, args.status, args.sha, token)
+
     elif args.pr_number and args.pr_comment:
         task_failed |= post_pr_comment(args.pr_number, args.pr_comment, token)
 
