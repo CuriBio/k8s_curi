df5e7c86beef70b6aba94686435cab485271d2a3
Tanner Peterson <tannermp23@hotmail.com>
2022-09-06 15:40:44 -0700

    delete sample dep

--- deployments/sample/services/test/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "test_service_ecr" {
-  description = "ECR url"
-  value = aws_ecr_repository.test_service_ecr.repository_url
-}
-
-output "remote_test" {
-  description = "test remote state"
-  value = data.terraform_remote_state.state1.outputs.vpc_id
-}
