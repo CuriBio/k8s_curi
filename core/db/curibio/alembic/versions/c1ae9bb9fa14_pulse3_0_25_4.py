642744ed97f944b2e836b70cfa2598bb2eede4f3
luciipak <luciypak@gmail.com>
2022-09-27 11:18:56 +1000

    merged main

---
+++ core/db/curibio/alembic/versions/c1ae9bb9fa14_pulse3_0_25_4.py
@@ -0,0 +1,25 @@
+"""pulse3 0.25.4
+
+Revision ID: c1ae9bb9fa14
+Revises: 871f6d005d86
+Create Date: 2022-09-23 10:57:43.588260
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "c1ae9bb9fa14"
+down_revision = "871f6d005d86"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    query = "INSERT INTO pulse3d_versions (version) VALUES ('0.25.4')"
+    op.execute(query)
+
+
+def downgrade():
+    query = "DELETE FROM pulse3d_versions WHERE version == '0.25.4'"
+    op.execute(query)
