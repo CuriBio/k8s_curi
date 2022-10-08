5cf7d2ef2f5e5d673ca4d72740b7720dda70bf8f
luciipak <luciypak@gmail.com>
2022-10-08 11:21:25 +1100

    added catch all latest tag

--- bin/curi_builder/builder/__init__.py
+++ bin/curi_builder/builder/__init__.py
@@ -82,9 +82,15 @@ def find_changed_svcs(sha: str):
             svc = "pulse3d_api"
 
         # need to output the pulse3d package version so it can be included in the name of the pulse3d-worker docker image
-        version = parse_py_dep_version(ch_path, "pulse3d") if svc == "pulse3d-worker" else get_svc_version(ch_path)
+        if svc == "pulse3d-worker": 
+            version = parse_py_dep_version(ch_path, "pulse3d") 
+        elif dep_type == "deployments":
+            # get version from service config to tag docker images
+            version = get_svc_version(ch_path)
+        else:
+            # protecting any missed changes
+            version = "latest"
         
-
         list_to_return.append({"path": ch_path, "deployment": dep_name, "service": svc, "version": version})
 
     return list_to_return
