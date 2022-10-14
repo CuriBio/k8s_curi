c99093757ccbb1a9e46abf7c0f037711b1d13fff
Tanner Peterson <tannermp23@hotmail.com>
2022-10-14 15:37:15 -0700

    fix alembic branch issue

--- core/db/curibio/alembic/versions/705b8a7903c8_pulse3d_0_27_0.py
+++ core/db/curibio/alembic/versions/705b8a7903c8_pulse3d_0_27_0.py
@@ -1,7 +1,7 @@
 """pulse3d 0.27.0
 
 Revision ID: 705b8a7903c8
-Revises: c1ae9bb9fa14
+Revises: 064b7f68d6c4
 Create Date: 2022-10-06 10:42:13.286377
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "705b8a7903c8"
-down_revision = "c1ae9bb9fa14"
+down_revision = "064b7f68d6c4"
 branch_labels = None
 depends_on = None
 
