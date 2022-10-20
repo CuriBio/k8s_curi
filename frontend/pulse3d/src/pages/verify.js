a8d7c66aced257528767bad83bd7b20209077146
Luci Pak <84344924+luciipak@users.noreply.github.com>
2022-10-21 10:54:13 +1100

    Added modal for successful account verification (#265)
    * added modal for successful account verification
    
    * updated comment

--- frontend/pulse3d/src/pages/verify.js
+++ frontend/pulse3d/src/pages/verify.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import CircularSpinner from "@/components/basicWidgets/CircularSpinner";
 import ModalWidget from "@/components/basicWidgets/ModalWidget";
 
 // TODO eventually need to find a better to way to handle some of these globally to use across app
@@ -13,37 +12,23 @@ const BackgroundContainer = styled.div`
   height: 100%;
 `;
 
-const ModalContainer = styled.div(
-  ({ user }) => `
-  height: ${user ? "460px" : "380px"};
-  width: 450px;
-  background-color: var(--light-gray);
-  position: relative;
-  border-radius: 3%;
-  overflow: hidden;
-  box-shadow: 0px 5px 5px -3px rgb(0 0 0 / 30%),
-    0px 8px 10px 1px rgb(0 0 0 / 20%), 0px 3px 14px 2px rgb(0 0 0 / 12%);
-`
-);
+const modalLabels = {
+  success: {
+    header: "Success!",
+    labels: ["Your account is now ready to be used, please proceed to the login page."],
+    buttons: ["Continue"],
+  },
+  error: {
+    header: "Error Occurred!",
+    labels: ["Something went wrong while attempting to verify your account."],
+    buttons: ["Close"],
+  },
+};
 
-const Label = styled.div`
-  font-size: 25px;
-  position: relative;
-  width: 100%;
-  text-align: center;
-  margin-top: 15px;
-`;
-
-const SpinnerContainer = styled.div`
-  height: 80%;
-  display: flex;
-  align-items: center;
-  position: relative;
-  justify-content: center;
-`;
 export default function Verify() {
   const router = useRouter();
-  const [openErrorModal, setOpenErrorModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalToDisplay, setModalToDisplay] = useState(modalLabels.success);
 
   useEffect(() => {
     if (router.pathname.includes("verify") && router.query.token) {
@@ -51,6 +36,7 @@ export default function Verify() {
       // this removes token param from url to make it not visible to users
       router.replace("/verify", undefined, { shallow: true });
     }
+    console.log("here");
   }, [router]);
 
   const verifyEmail = async ({ token }) => {
@@ -65,39 +51,33 @@ export default function Verify() {
         headers,
       });
 
-      if (res.status === 204) {
-        // once verified, auto redirect user to login page
-        router.replace("/login", undefined, { shallow: true });
-      } else {
-        // else open error modal to let user know it didn't work
-        setOpenErrorModal(true);
-      }
+      res.status === 204 ? setModalToDisplay(modalLabels.success) : setModalToDisplay(modalLabels.error);
+
+      // open modal after setting correct labels
+      setOpenModal(true);
     } catch (e) {
       console.log(`ERROR verifying new user account: ${e}`);
       // if error, open error modal to let user know it didn't work
-      setOpenErrorModal(true);
+      setModalToDisplay(modalLabels.error);
+      setOpenModal(true);
     }
   };
 
-  const closeErrorModal = () => {
-    setOpenErrorModal(false);
+  const closeModal = () => {
+    setOpenModal(false);
     // redirect user to login page to exit verify page regardless of outcome
     router.replace("/login", undefined, { shallow: true });
   };
 
   return (
     <BackgroundContainer>
-      <ModalContainer>
-        <Label>Verifying...</Label>
-        <SpinnerContainer>
-          <CircularSpinner size={200} />
-        </SpinnerContainer>
-      </ModalContainer>
       <ModalWidget
-        open={openErrorModal}
-        closeModal={closeErrorModal}
-        header="Error Occurred!"
-        labels={["Something went wrong while attempting to verify your account."]}
+        width={500}
+        open={openModal}
+        closeModal={closeModal}
+        header={modalToDisplay.header}
+        labels={modalToDisplay.labels}
+        buttons={modalToDisplay.buttons}
       />
     </BackgroundContainer>
   );
