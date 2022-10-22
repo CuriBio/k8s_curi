7b76d49057f2a216b31bac9936e1cc43d2600f67
luciipak <luciypak@gmail.com>
2022-10-22 15:11:21 +1100

    added customer id to migration

--- core/db/curibio/alembic/versions/4aac7b465d29_add_cloud_usage_column_to_customers_.py
+++ core/db/curibio/alembic/versions/4aac7b465d29_add_cloud_usage_column_to_customers_.py
@@ -7,23 +7,35 @@ Create Date: 2022-10-19 10:39:53.261901
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+from sqlalchemy.dialects import postgresql as pg
 
 # revision identifiers, used by Alembic.
-revision = '4aac7b465d29'
-down_revision = '705b8a7903c8'
+revision = "4aac7b465d29"
+down_revision = "705b8a7903c8"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column("customers", sa.Column("usage_restrictions", postgresql.JSONB, server_default="{}", nullable=True))
-    # default all existing to free pulse3d users
-    op.execute("UPDATE users SET data = '{ "scope": ["pulse3d:user:free"] }' ")
+    op.add_column("customers", sa.Column("usage_restrictions", pg.JSONB, server_default="{}", nullable=True))
 
-    op.execute("UPDATE customers SET data = '{ "scope": ["pulse3d:customer:free"] }' AND usage_restrictions = '{"pulse3d": {"uploads": "10", "jobs": "20"}}'")
+    # create foreign key contraints for tables that have customer_id column
+    for table in ("uploads", "jobs_result"):
+        op.add_column(table, sa.Column("customer_id", pg.UUID(as_uuid=True), nullable=True))
+        op.create_foreign_key(f"fk_{table}_customers", table, "customers", ["customer_id"], ["id"])
+
+    op.execute(
+        "UPDATE uploads SET customer_id=(SELECT customer_id from users where uploads.user_id=users.id)"
+    )
+    op.execute(
+        "UPDATE jobs_result SET customer_id=(SELECT customer_id from uploads where jobs_result.upload_id=uploads.id)"
+    )
 
 
 def downgrade():
     op.drop_column("customers", "usage_restrictions")
-    op.execute("UPDATE users SET data = '{ "scope": ["users:free"] }' ")
+
+    # remove foreign key constraint in users, mantarray_recording_sessions, mantarray_session_log_files
+    for table in ("uploads", "jobs_result"):
+        op.drop_constraint(f"fk_{table}_customers", table)
+        op.drop_column(table, "customer_id")
