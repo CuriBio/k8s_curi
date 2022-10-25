6c8e553c586b74a1e5b33ce056487092ad19cc63
luciipak <luciypak@gmail.com>
2022-10-26 08:48:57 +1100

    cleanup

--- core/lib/jobs/jobs/jobs.py
+++ core/lib/jobs/jobs/jobs.py
@@ -221,6 +221,7 @@ def _get_placeholders_str(num_placeholders, start=1):
 async def check_customer_quota(con, customer_id, service) -> Dict[str, bool]:
     """Query DB for service-specific customer account usage.
 
+    Will be called for all account tiers, unlimited has a value of -1.
     Returns:
         - Dictionary containing boolean values for if uploads or job quotas have been reached
     """
@@ -230,7 +231,6 @@ async def check_customer_quota(con, customer_id, service) -> Dict[str, bool]:
         usage_query = "select usage_restrictions->$1 as usage from customers where id=$2"
         usage_json = await con.fetchrow(usage_query, service, customer_id)
         usage_dict = json.loads(usage_json["usage"])
-
         # grab total uploads and jobs of customer for specific service
         jobs_type = "mantarray"  # REMEMBER TO CHANGE BACK AFTER MIGRATING TO USE PULSE3D
         query = "select count(*) from jobs_result where customer_id=$1 and type=$2 group by upload_id"
@@ -238,7 +238,7 @@ async def check_customer_quota(con, customer_id, service) -> Dict[str, bool]:
         total_uploads = len(customer_data)
         total_jobs = sum(customer_data)
 
-        # return boolean values if reached
+        # return boolean values if reached, -1 means infinite uploads/jobs allowed for paid account
         return {
             "uploads_reached": total_uploads >= usage_dict["uploads"] and usage_dict["uploads"] != -1,
             "jobs_reached": total_jobs >= usage_dict["jobs"] and usage_dict["jobs"] != -1,
