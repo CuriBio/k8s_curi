f035e77399c5ac09d28ac90e54d069159d595c44
nikitapoly <nikitapoly@comcast.net>
2022-10-24 17:44:14 -0700

    filters can now filter dates

--- frontend/pulse3d/src/pages/uploads.js
+++ frontend/pulse3d/src/pages/uploads.js
@@ -134,7 +134,7 @@ export default function Uploads() {
   const [selectedAnalysis, setSelectedAnalysis] = useState();
   const [pending, setPending] = useState(true);
   const [filterString, setFilterString] = useState("");
-  const [filtercolumn, setFilterColumn] = useState("");
+  const [filterColumn, setFilterColumn] = useState("");
 
   const uploadTableColumns = [
     {
@@ -188,25 +188,25 @@ export default function Uploads() {
           Owner: "username",
           Recording: "name",
           ID: "id",
-          "Date Created": "createdAt",
-          "Last Analyzed": "lastAnalyzed",
+          Date: "createdAt",
+          Analyzed: "lastAnalyzed",
         }
       : {
           Recording: "name",
           ID: "id",
-          "Date Created": "createdAt",
-          "Last Analyzed": "lastAnalyzed",
+          Date: "createdAt",
+          Analyzed: "lastAnalyzed",
         };
 
-  const filterColumn = () => {
+  const filterColumns = () => {
     return rows.filter((row) => {
-      return row[toFilterField[filtercolumn]].toLocaleLowerCase().includes(filterString.toLocaleLowerCase());
+      return row[toFilterField[filterColumn]].toLocaleLowerCase().includes(filterString.toLocaleLowerCase());
     });
   };
   //when filter string changes, refilter results
   useEffect(() => {
-    if (filtercolumn) {
-      const newList = filterColumn();
+    if (filterColumn) {
+      const newList = filterColumns();
       setDisplayRows(newList);
     }
   }, [filterString]);
@@ -311,8 +311,8 @@ export default function Uploads() {
     setRows([...formattedUploads]);
     setDisplayRows([...formattedUploads]);
 
-    if (filtercolumn) {
-      const newList = filterColumn();
+    if (filterColumn) {
+      const newList = filterColumns();
       setDisplayRows(newList);
     }
   }, [jobs]);
@@ -632,6 +632,7 @@ export default function Uploads() {
               expandableRowsComponent={ExpandedComponent}
               customStyles={customStyles}
               progressPending={pending}
+              defaultSortFieldId={5}
               progressComponent={
                 <SpinnerContainer>
                   <CircularSpinner size={200} color={"secondary"} />
