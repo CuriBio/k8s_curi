a79a53099d672df27430fb8d4a481aed1a93d719
luciipak <luciypak@gmail.com>
2022-11-16 09:56:52 +1100

    basic change of register scopes

--- core/lib/auth/auth/scopes.py
+++ core/lib/auth/auth/scopes.py
@@ -1,3 +1,3 @@
 CUSTOMER_SCOPES = frozenset(["customer:free", "customer:paid"])
-PULSE3D_USER_SCOPES = frozenset(["pulse3d:free", "pulse3d:paid"])
+PULSE3D_USER_SCOPES = frozenset(["pulse3d:free", "pulse3d:paid", "pulse3d:rw_all"])
 PULSE3D_SCOPES = frozenset([*CUSTOMER_SCOPES, *PULSE3D_USER_SCOPES])
