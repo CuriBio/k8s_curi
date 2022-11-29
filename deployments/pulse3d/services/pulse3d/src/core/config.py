26fe424c40046c33a6f7f4df26f09fda67550ac4
luciipak <luciypak@gmail.com>
2022-11-29 15:15:44 +1100

    removed local env files

--- deployments/pulse3d/services/pulse3d/src/core/config.py
+++ deployments/pulse3d/services/pulse3d/src/core/config.py
@@ -1,7 +1,7 @@
 from starlette.config import Config
 from starlette.datastructures import Secret
 
-config = Config("/Users/lucipak/Documents/gpg/.env_localhost")
+config = Config(".env")
 # version used to tag the docker image
 VERSION = "0.4.0"
 
