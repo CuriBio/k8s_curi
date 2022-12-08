e571fe0f23f7af1dcd1ff3b5a784b22ad3938569
Luci Pak <84344924+luciipak@users.noreply.github.com>
2022-12-08 14:03:26 +1100

    Alembic branching (#334)
    * fixed alembic versions
    
    * fixed comments

--- core/db/curibio/alembic/versions/04c57d3482e3_add_pulse_0_28_1.py
+++ core/db/curibio/alembic/versions/04c57d3482e3_add_pulse_0_28_1.py
@@ -1,17 +1,16 @@
 """add pulse 0.28.1
 
 Revision ID: 04c57d3482e3
-Revises: 323696292448
+Revises: e7b1c23cdb33
 Create Date: 2022-11-29 14:23:28.198412
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
 revision = "04c57d3482e3"
-down_revision = "323696292448"
+down_revision = "e7b1c23cdb33"
 branch_labels = None
 depends_on = None
 
