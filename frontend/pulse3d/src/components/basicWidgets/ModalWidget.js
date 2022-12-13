e1994ceee851116dfed00b28206b6201b771f571
nikitapoly <nikitapoly@comcast.net>
2022-12-12 17:54:49 -0800

    added modal

--- frontend/pulse3d/src/components/basicWidgets/ModalWidget.js
+++ frontend/pulse3d/src/components/basicWidgets/ModalWidget.js
@@ -52,10 +52,10 @@ export default function ModalWidget({
   const [disabled, setDisabled] = useState(false);
 
   useEffect(() => {
-    // whem modal labels change, make sure to reset disabled buttons
+    // whem modal toggles open change, make sure to reset disabled buttons
     // important when a series of modals is used and just labels change
     setDisabled(false);
-  }, [labels]);
+  }, [open, labels]);
 
   return (
     <div>
