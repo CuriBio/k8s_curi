642046a9154f76d6e20fbc8ec554a2bd73a04c3a
luciipak <luciypak@gmail.com>
2022-12-22 14:24:18 +1100

    immutabledict

--- core/lib/auth/auth/scopes.py
+++ core/lib/auth/auth/scopes.py
@@ -1,5 +1,7 @@
+from immutabledict import immutabledict
+
 CUSTOMER_SCOPES = frozenset(["customer:free", "customer:paid"])
 PULSE3D_USER_SCOPES = frozenset(["pulse3d:free", "pulse3d:paid", "pulse3d:rw_all_data"])
 PULSE3D_SCOPES = frozenset([*CUSTOMER_SCOPES, *PULSE3D_USER_SCOPES])
 ACCOUNT_SCOPES = frozenset(["users:verify", "users:reset", "customer:reset"])
-PULSE3D_PAID_USAGE = {"pulse3d": {"uploads": -1, "jobs": -1}}
+PULSE3D_PAID_USAGE = immutabledict({"pulse3d": {"uploads": -1, "jobs": -1}})
