b8a847098694771e3422c32b3bbf50b6a97e3c3b
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2023-01-23 12:30:15 -0800

    fix alembic script adding pulse3d v0.29.1 (#366)

--- core/db/curibio/alembic/versions/d7a72568d90b_pulse3d_version_0_29_1.py
+++ core/db/curibio/alembic/versions/d7a72568d90b_pulse3d_version_0_29_1.py
@@ -6,7 +6,6 @@ Create Date: 2023-01-19 15:02:57.165859
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -18,9 +17,7 @@ depends_on = None
 
 def upgrade():
     op.execute("INSERT INTO pulse3d_versions (version, state) VALUES ('0.29.1', 'external')")
-    op.execute("UPDATE pulse3d_versions SET state='deprecated' WHERE version='0.29.1'")
 
 
 def downgrade():
     op.execute("DELETE FROM pulse3d_versions WHERE version='0.29.1'")
-    op.execute("UPDATE pulse3d_versions SET state='external' WHERE version='0.29.1'")
