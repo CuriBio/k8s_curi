3eae285052946591bc9bbbdc84e2ef34eb27f58c
Tanner Peterson <tannermp23@hotmail.com>
2023-03-13 22:49:33 -0700

    update pulse3d api

--- deployments/pulse3d/services/pulse3d/tests/test_main.py
+++ deployments/pulse3d/services/pulse3d/tests/test_main.py
@@ -624,6 +624,9 @@ def test_jobs__post__basic_params_given(mocker, mocked_asyncpg_con):
 def test_jobs__post__correctly_updates_peak_valley_indices_based_on_differing_pulse3d_versions_greater_than_0_28_2(
     mocker, mocked_asyncpg_con, previous_version, version, expected_diff
 ):
+    mocker.patch.object(main, "create_job", autospec=True, return_value=uuid.uuid4())
+    mocker.patch.object(main, "upload_file_to_s3", autospec=True)
+    spied_dataframe = mocker.spy(pd, "DataFrame")
 
     initial_peaks_valleys = {"A1": [[1, 3, 5], [2, 4]], "B1": [[6, 8], [7]]}
     test_analysis_params = {
@@ -631,13 +634,10 @@ def test_jobs__post__correctly_updates_peak_valley_indices_based_on_differing_pu
         "version": version,
         "peaks_valleys": initial_peaks_valleys,
     }
-    test_user_id = uuid.uuid4()
 
+    test_user_id = uuid.uuid4()
     access_token = get_token(scope=["pulse3d:free"], userid=test_user_id, account_type="user")
-    mocker.patch.object(main, "create_job", autospec=True, return_value=uuid.uuid4())
     mocked_asyncpg_con.fetchrow.return_value = {"user_id": test_user_id}
-    mocker.patch.object(main, "upload_file_to_s3", autospec=True)
-    spied_dataframe = mocker.spy(pd, "DataFrame")
 
     mocker.patch.object(
         main,
@@ -687,6 +687,8 @@ def test_jobs__post__correctly_updates_peak_valley_indices_based_on_differing_pu
 def test_jobs__post__correctly_updates_peak_valley_indices_based_on_differing_pulse3d_versions_less_than_0_28_2(
     mocker, mocked_asyncpg_con, previous_version, version, expected_peaks_valleys
 ):
+    mocked_create_job = mocker.patch.object(main, "create_job", autospec=True, return_value=uuid.uuid4())
+    mocker.patch.object(main, "upload_file_to_s3", autospec=True)
 
     initial_peaks_valleys = {"A1": [[1, 3, 5], [2, 4]], "B1": [[6, 8], [7]]}
     test_analysis_params = {
@@ -694,22 +696,19 @@ def test_jobs__post__correctly_updates_peak_valley_indices_based_on_differing_pu
         "version": version,
         "peaks_valleys": initial_peaks_valleys,
     }
+
     test_user_id = uuid.uuid4()
     test_customer_id = uuid.uuid4()
     access_token = get_token(
         scope=["pulse3d:free"], userid=test_user_id, account_type="user", customer_id=test_customer_id
     )
-    mocked_create_job = mocker.patch.object(main, "create_job", autospec=True, return_value=uuid.uuid4())
     mocked_asyncpg_con.fetchrow.return_value = {"user_id": test_user_id}
 
     mocker.patch.object(
         main,
         "check_customer_quota",
         return_value={
-            "current": {
-                "uploads": "0",
-                "jobs": "0",
-            },
+            "current": {"uploads": "0", "jobs": "0"},
             "jobs_reached": False,
             "limits": {"expiration_date": "", "jobs": "-1", "uploads": "-1"},
             "uploads_reached": False,
@@ -979,9 +978,8 @@ def test_jobs__post__with_baseline_widths_to_use(param_tuple, mocked_asyncpg_con
     assert mocked_create_job.call_args[1]["meta"]["analysis_params"] == expected_analysis_params
 
 
-@pytest.mark.parametrize(
-    "version", ["0.24.6", "0.25.0", "0.25.2", "0.25.4", "0.26.0", "0.28.0", "0.28.2", "0.28.3"]
-)
+# Tanner (3/13/23): only really need to test versions that are live in prod are being tested in test cluster
+@pytest.mark.parametrize("version", ["0.25.2", "0.25.4", "0.28.0", "0.28.2", "0.28.3", "0.30.4", "0.30.4"])
 def test_jobs__post__omits_analysis_params_not_supported_by_the_selected_pulse3d_version(
     version, mocked_asyncpg_con, mocker
 ):
@@ -1025,12 +1023,15 @@ def test_jobs__post__omits_analysis_params_not_supported_by_the_selected_pulse3d
     if pulse3d_semver >= "0.25.4":
         expected_analysis_param_keys.append("normalize_y_axis")
     if pulse3d_semver >= "0.28.1":
-        expected_analysis_param_keys.append(
-            "include_stim_protocols",
-        )
+        expected_analysis_param_keys.append("include_stim_protocols")
     if pulse3d_semver >= "0.30.1":
         expected_analysis_param_keys.append("stiffness_factor")
         expected_analysis_param_keys.append("inverted_post_magnet_wells")
+    if pulse3d_semver >= "0.30.3":
+        expected_analysis_param_keys.append("well_groups")
+    if pulse3d_semver >= "0.30.5":
+        expected_analysis_param_keys.append("stim_waveform_format")
+
     if "0.25.2" <= pulse3d_semver <= "0.28.0":
         expected_analysis_param_keys.append("peaks_valleys")
 
@@ -1384,8 +1385,13 @@ def test_waveform_data__get__getting_job_metadata_from_db_errors(mocker):
 
 @pytest.mark.parametrize("pulse3d_version", [None, "1.2.3"])
 def test_waveform_data__get__time_force_parquet_found(mocker, pulse3d_version):
+    mocker.patch.object(main, "glob", return_value=["/tmp/directory/recording"], autospec=True)
+    mocker.patch.object(main, "download_directory_from_s3", autospec=True)
+
     test_user_id = uuid.uuid4()
+
     test_inclusive_df = pd.DataFrame()
+    mocker.patch.object(pd, "read_parquet", return_value=test_inclusive_df, autospec=True)
 
     test_data = list(range(5))
     expected_data = test_data[:-1]
@@ -1397,9 +1403,6 @@ def test_waveform_data__get__time_force_parquet_found(mocker, pulse3d_version):
         elif column != "A1__raw":
             series[test_data[-1]] = np.nan
         test_inclusive_df[column] = series
-
-    mocker.patch.object(main, "glob", return_value=["/tmp/directory/recording"], autospec=True)
-    mocker.patch.object(pd, "read_parquet", return_value=test_inclusive_df, autospec=True)
 
     expected_analysis_params = {
         param: None
