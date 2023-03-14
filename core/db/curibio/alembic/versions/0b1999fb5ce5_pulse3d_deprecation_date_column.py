74bf35bdecdb5099bc906026a635b3c7c092927e
nikitapoly <nikitapoly@comcast.net>
2023-03-14 10:43:05 -0700

    added query for deprecation of older versions

--- core/db/curibio/alembic/versions/0b1999fb5ce5_pulse3d_deprecation_date_column.py
+++ core/db/curibio/alembic/versions/0b1999fb5ce5_pulse3d_deprecation_date_column.py
@@ -6,7 +6,6 @@ Create Date: 2023-03-10 12:11:27.147725
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -20,7 +19,10 @@ def upgrade():
     # can not remove this new enum value unless deleting it and remaking a new enum type
     op.execute("ALTER TYPE WorkerState ADD VALUE 'removed'")
     op.execute("ALTER TABLE pulse3d_versions ADD COLUMN end_of_life_date DATE DEFAULT null")
+    # deprecate all versions of pulse3d before 0.28.2 inclusive
+    op.execute("UPDATE pulse3d_versions SET state = 'deprecated' WHERE version <= '0.28.2'")
 
 
 def downgrade():
     op.execute("ALTER TABLE pulse3d_versions DROP COLUMN end_of_life_date")
+    op.execute("UPDATE pulse3d_versions SET state = 'external' WHERE version <= '0.28.2'")
