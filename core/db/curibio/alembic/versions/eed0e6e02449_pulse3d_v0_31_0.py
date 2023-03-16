de5bdaea930b2e437e9c0e76e3de4baa482835c4
Jason Person <jrperson@gmail.com>
2023-03-16 13:14:55 -0700

    enable pulse3d v0.31.0

---
+++ core/db/curibio/alembic/versions/eed0e6e02449_pulse3d_v0_31_0.py
@@ -0,0 +1,24 @@
+"""pulse3d v0.31.0
+
+Revision ID: eed0e6e02449
+Revises: b1c798d03073
+Create Date: 2023-03-16 13:04:53.372826
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eed0e6e02449'
+down_revision = 'b1c798d03073'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("INSERT INTO pulse3d_versions (version, state) VALUES ('0.31.', 'external')")
+
+
+def downgrade():
+    op.execute("DELETE FROM pulse3d_versions WHERE version='0.31.0'")
