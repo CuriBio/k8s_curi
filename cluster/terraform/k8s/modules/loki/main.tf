9eb7db16c59e3b38fe1ca7fb1762df0674872a75
luciipak <luciypak@gmail.com>
2023-03-18 14:36:31 +1100

    working grafana and loki setup, missing s3

--- cluster/terraform/k8s/modules/loki/main.tf
+++ cluster/terraform/k8s/modules/loki/main.tf
@@ -1,3 +1,18 @@
-resource "aws_s3_bucket" "loki_object_store" {
-  bucket = "curi-${var.cluster_name}-loki-logs"
+resource "aws_s3_bucket" "loki_logs_bucket" {
+  bucket = "${var.cluster_name}-loki-logs"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "loki_logs_bucket" {
+  bucket = aws_s3_bucket.loki_logs_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "loki_logs_bucket" {
+  bucket = aws_s3_bucket.loki_logs_bucket.id
+  acl    = "private"
 }
