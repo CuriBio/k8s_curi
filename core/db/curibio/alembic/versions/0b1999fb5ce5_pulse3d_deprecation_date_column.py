c520c7657f3fc33f5a83f4a60d31ce92fae00d28
Tanner Peterson <tannermp23@hotmail.com>
2023-03-20 14:46:30 -0700

    fix DB migrations

--- core/db/curibio/alembic/versions/0b1999fb5ce5_pulse3d_deprecation_date_column.py
+++ core/db/curibio/alembic/versions/0b1999fb5ce5_pulse3d_deprecation_date_column.py
@@ -16,10 +16,11 @@ depends_on = None
 
 
 def upgrade():
-    op.execute(
-        "ALTER TABLE pulse3d_versions ADD CONSTRAINT deprecated_version_must_have_end_of_life_date CHECK(status <> 'deprecated' OR end_of_life_date IS NOT NULL)"
-    )
     op.execute("ALTER TABLE pulse3d_versions ADD COLUMN end_of_life_date DATE DEFAULT null")
+    op.execute("UPDATE pulse3d_versions SET end_of_life_date='2024-01-01' WHERE state='deprecated'")
+    op.execute(
+        "ALTER TABLE pulse3d_versions ADD CONSTRAINT deprecated_version_must_have_end_of_life_date CHECK(state != 'deprecated' OR end_of_life_date IS NOT NULL)"
+    )
 
 
 def downgrade():
