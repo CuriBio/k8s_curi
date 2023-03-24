a4a7337966a291895e675d092384e0946d6842f8
NikitaPoly <46114925+NikitaPoly@users.noreply.github.com>
2023-03-24 16:06:06 -0700

    adding new pulse3d version (#461)
    * adding new pulse3d version
    
    * adding pulse3d 0.32.2
    
    * removed extra files
    
    * typos
    
    * version bumps and format
    
    * pulse3d 0.32.1 deprecation

---
+++ core/db/curibio/alembic/versions/04bfacd4385d_pulse_0_32_2.py
@@ -0,0 +1,23 @@
+"""pulse 0.32.2
+
+Revision ID: 04bfacd4385d
+Revises: 0b1999fb5ce5
+Create Date: 2023-03-21 14:53:49.083859
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "04bfacd4385d"
+down_revision = "0b1999fb5ce5"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("INSERT INTO pulse3d_versions (version, state) VALUES ('0.32.2', 'testing')")
+
+
+def downgrade():
+    op.execute("DELETE FROM pulse3d_versions WHERE version='0.32.2'")
