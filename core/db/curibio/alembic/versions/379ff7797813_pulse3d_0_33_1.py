8e1725338addbed9a09a24439781e83589c0d6e5
Luci Pak <luciypak@gmail.com>
2023-05-05 14:25:14 +1000

    pulse3d 0.33.1

---
+++ core/db/curibio/alembic/versions/379ff7797813_pulse3d_0_33_1.py
@@ -0,0 +1,23 @@
+"""pulse3d 0.33.1
+
+Revision ID: 379ff7797813
+Revises: 6ae5abb40525
+Create Date: 2023-05-05 14:22:13.366376
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "379ff7797813"
+down_revision = "6ae5abb40525"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("INSERT INTO pulse3d_versions (version, state) VALUES ('0.33.1', 'external')")
+
+
+def downgrade():
+    op.execute("DELETE FROM pulse3d_versions WHERE version='0.33.1'")
