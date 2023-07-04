5ad8713151263895f0daa6a5bd017c38b42a3a3f
Tanner Peterson <tannermp23@hotmail.com>
2023-07-04 09:31:16 -0700

    fix svc version bump

--- deployments/apiv2/services/mantarray/src/core/config.py
+++ deployments/apiv2/services/mantarray/src/core/config.py
@@ -3,7 +3,7 @@ from starlette.datastructures import Secret
 
 config = Config(".env")
 # version used to tag the docker image
-VERSION = "0.5.1"
+VERSION = "0.5.0"
 
 CLUSTER_NAME = config("CLUSTER_NAME", cast=str)
 POSTGRES_USER = config("POSTGRES_USER", cast=str)
