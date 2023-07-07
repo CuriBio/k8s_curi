4a9fac22b4d55075e5f6b3caf11749ef96c133cc
Luci Pak <luciypak@gmail.com>
2023-07-08 09:43:33 +1000

    replace 0.33.9 with 0.33.11

--- frontend/pulse3d/src/components/interactiveAnalysis/InteractiveAnalysisModal.js
+++ frontend/pulse3d/src/components/interactiveAnalysis/InteractiveAnalysisModal.js
@@ -894,7 +894,7 @@ export default function InteractiveWaveformModal({
         if (Object.keys(waveformData).length === 24) {
           return true;
         } else {
-          const minVersion = Object.keys(waveformData).length < 24 ? "0.32.2" : "0.33.9";
+          const minVersion = Object.keys(waveformData).length < 24 ? "0.32.2" : "0.33.11";
           return semverGte(v, minVersion);
         }
       });
