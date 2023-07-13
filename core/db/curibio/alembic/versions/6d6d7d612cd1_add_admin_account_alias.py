7a97f6b12adbcf07dc3317c1e7dd775e47c2e3ee
nikitapoly <nikitapolytech@gmail.com>
2023-07-12 17:07:18 -0700

    alembic migration

---
+++ core/db/curibio/alembic/versions/6d6d7d612cd1_add_admin_account_alias.py
@@ -0,0 +1,23 @@
+"""add admin account alias
+
+Revision ID: 6d6d7d612cd1
+Revises: c3ab9e9629ce
+Create Date: 2023-07-12 16:44:49.087101
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "6d6d7d612cd1"
+down_revision = "c3ab9e9629ce"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("ALTER TABLE customers ADD COLUMN id_alias VARCHAR(255)")
+
+
+def downgrade():
+    op.execute("ALTER TABLE customers DROP COLUMN id_alias")
