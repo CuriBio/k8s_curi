c7016f3749d266a44652be19f1fe1537e379b852
Luci Pak <luciypak@gmail.com>
2023-08-10 13:09:16 +1000

    fix migration typo

---
+++ core/db/curibio/alembic/versions/316e4d5eb991_add_failed_pass_attempts_column.py
@@ -0,0 +1,28 @@
+"""add failed pass attempts column
+
+Revision ID: 316e4d5eb991
+Revises: 4c5b40dd7e96
+Create Date: 2023-07-27 10:17:29.263357
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "316e4d5eb991"
+down_revision = "4c5b40dd7e96"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    for table in ("customers", "users"):
+        op.add_column(
+            table, sa.Column("failed_login_attempts", sa.INTEGER(), server_default="0", nullable=False)
+        )
+
+
+def downgrade():
+    for table in ("customers", "users"):
+        op.drop_column(table, "failed_login_attempts")
