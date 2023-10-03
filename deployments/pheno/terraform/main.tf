1d6f1a1e11e38374e8d845b6c1a1d576db201968
Luci Pak <luciypak@gmail.com>
2023-10-03 11:31:43 +1100

    add s3 policy to remaining services

--- deployments/pheno/terraform/main.tf
+++ deployments/pheno/terraform/main.tf
@@ -36,6 +36,30 @@ resource "aws_s3_bucket" "phenolearn" {
   bucket = "phenolearn"
 }
 
+resource "aws_s3_bucket_policy" "phenolearn" {
+    bucket = aws_s3_bucket.phenolearn.id
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid       = "EnforceTls"
+                Effect    = "Deny"
+                Principal = "*"
+                Action    = "s3:*"
+                Resource = [
+                    "${aws_s3_bucket.phenolearn.arn}/*",
+                    "${aws_s3_bucket.phenolearn.arn}",
+                ]
+                Condition = {
+                    Bool = {
+                        "aws:SecureTransport" = "false"
+                    }
+                }
+            },
+        ]
+    })
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "phenolearn" {
   bucket = aws_s3_bucket.phenolearn.bucket
 
