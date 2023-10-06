f3fe096d172e0aa93c7950e79939818397aaa78a
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2023-10-06 15:38:20 -0700

    Ecr lifecycle (#661)
    - add lifecycle policy to ecr repos

--- deployments/apiv2/terraform/users/main.tf
+++ deployments/apiv2/terraform/users/main.tf
@@ -6,3 +6,39 @@ resource "aws_ecr_repository" "users_ecr_repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "users_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.users_ecr_repo.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 3 tagged images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["0"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep only 1 untagged image",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
