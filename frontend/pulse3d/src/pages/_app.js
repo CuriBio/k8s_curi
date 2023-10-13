9f66c0d16174e76eddf747a881920e2e683d6b7c
Luci Pak <luciypak@gmail.com>
2023-10-13 11:58:55 +1100

    remove console.log

--- frontend/pulse3d/src/pages/_app.js
+++ frontend/pulse3d/src/pages/_app.js
@@ -104,7 +104,7 @@ function Pulse({ Component, pageProps }) {
             }
           } else if (data.msgType === "authCheck") {
             const newAccountInfo = data.accountInfo;
-            console.log(data);
+
             if (data.isLoggedIn) {
               setAvailableScopes({ customer: data.customerScopes, user: data.userScopes });
               setIsCuriAdmin(newAccountInfo.accountScope.find((scope) => scope === "curi:admin"));
