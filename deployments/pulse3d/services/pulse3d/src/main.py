55bff180c95e74bfc0453d801790e1fdee095bd1
Tanner Peterson <tannermp23@hotmail.com>
2024-02-16 16:31:41 -0800

    bump p3d version

--- deployments/pulse3d/services/pulse3d/src/main.py
+++ deployments/pulse3d/services/pulse3d/src/main.py
@@ -547,7 +547,7 @@ async def create_new_job(
             # TODO remove this once done testing rc versions of pulse3d rewrite
             version = details.version
             if version == "1.0.0":
-                version = "1.0.0rc26"
+                version = "1.0.0rc27"
 
             job_meta = {"analysis_params": analysis_params, "version": version}
             # if a name is present, then add to metadata of job
@@ -571,9 +571,9 @@ async def create_new_job(
                 rewrite_job_id = await create_job(
                     con=con,
                     upload_id=upload_id,
-                    queue="pulse3d-v1.0.0rc26",
+                    queue="pulse3d-v1.0.0rc27",
                     priority=priority,
-                    meta={**job_meta, "version": "1.0.0rc26"},
+                    meta={**job_meta, "version": "1.0.0rc27"},
                     customer_id=customer_id,
                     job_type=upload_type,
                     add_to_results=False,
