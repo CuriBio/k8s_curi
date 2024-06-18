66f59be9da202566f5a86de285bb13919237dcb7
Tanner Peterson <55764472+tannermpeterson@users.noreply.github.com>
2024-06-18 16:50:26 -0500

    IA clean up + Remove dups for single well (#823)
    - clean up IA tooltips + text
    - add ability to remove dups on a single well after changes are made

--- frontend/pulse3d/src/components/interactiveAnalysis/InteractiveAnalysisModal.js
+++ frontend/pulse3d/src/components/interactiveAnalysis/InteractiveAnalysisModal.js
@@ -474,6 +474,20 @@ export default function InteractiveWaveformModal({
 
       handleChangeForCurrentWell(ACTIONS.ADD, wellSettings, changelogMsg);
     },
+    removeDupsForWell: () => {
+      const wellSettings = customAnalysisSettings[selectedWell];
+      wellSettings.allFeatureIndices.peaks = removeDups(
+        wellSettings.allFeatureIndices.peaks,
+        wellSettings.duplicateFeatureIndices.peaks
+      );
+      wellSettings.allFeatureIndices.valleys = removeDups(
+        wellSettings.allFeatureIndices.valleys,
+        wellSettings.duplicateFeatureIndices.valleys
+      );
+      const changelogMsg = "Remove duplicates";
+
+      handleChangeForCurrentWell(ACTIONS.ADD, wellSettings, changelogMsg);
+    },
   };
 
   // One-time setup for component.
@@ -537,7 +551,7 @@ export default function InteractiveWaveformModal({
 
   // update customAnalysisSettingsForWell whenever customAnalysisSettings or the currently selected well changes
   useEffect(() => {
-    if (selectedWell in customAnalysisSettings)
+    if (selectedWell in customAnalysisSettings) {
       setCustomAnalysisSettingsForWell(
         // Tanner (6/1/23): Copying just to be safe
         deepCopy({
@@ -547,6 +561,7 @@ export default function InteractiveWaveformModal({
           thresholdEndpoints: customAnalysisSettings[selectedWell].thresholdEndpoints,
         })
       );
+    }
   }, [customAnalysisSettings, selectedWell]);
 
   const handleWaveformData = async () => {
@@ -983,12 +998,12 @@ export default function InteractiveWaveformModal({
       <OuterParamContainer>
         <ParamContainer>
           <ParamLabel htmlFor="removeDupFeatures">
-            Remove Duplicate Peaks/Valleys:
+            Remove Initial Duplicate Peaks/Valleys:
             <Tooltip
               title={
                 <TooltipText>
                   {
-                    "Automatically remove duplicate peaks and valleys from all wells and can only be performed when no other changes have been made."
+                    "Automatically remove duplicate peaks/valleys from all wells. Can only be performed when no other changes have been made."
                   }
                 </TooltipText>
               }
