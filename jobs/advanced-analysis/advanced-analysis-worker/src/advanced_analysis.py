b8fb1d9daba7e2e180b91eb17a4923053d9a82cb
Tanner Peterson <tannermp23@hotmail.com>
2024-08-22 14:52:32 -0700

    bump version

--- jobs/advanced-analysis/advanced-analysis-worker/src/advanced_analysis.py
+++ jobs/advanced-analysis/advanced-analysis-worker/src/advanced_analysis.py
@@ -12,7 +12,7 @@ from immutabledict import immutabledict
 import polars as pl
 from xlsxwriter import Workbook
 
-ADVANCED_ANALYSIS_VERSION = "0.1.0rc0"
+ADVANCED_ANALYSIS_VERSION = "0.1.0rc1"
 
 # TODO add logging
 
