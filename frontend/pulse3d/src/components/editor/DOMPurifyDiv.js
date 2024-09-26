1d25c5aca325066aa867dbe738388c0911266968
Roberto Araiza <167813555+roberto-araiza-curibio@users.noreply.github.com>
2024-09-26 10:52:01 -0700

    Switch editor from Quill to TinyMCE (#881)

--- frontend/pulse3d/src/components/editor/DOMPurifyDiv.js
+++
@@ -1,9 +0,0 @@
-import DOMPurify from "dompurify";
-
-export default function DOMPurifyDiv({ rawHTML }) {
-  return (
-    <div className="ql-editor">
-      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(rawHTML) }} />
-    </div>
-  );
-}
