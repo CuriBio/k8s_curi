fcfa370c8ab019c4018f2feb21f9ecf83f81c880
Roberto Araiza <167813555+roberto-araiza-curibio@users.noreply.github.com>
2024-09-30 16:57:25 -0700

    Change notification endpoint to also create per-[user|customer] notification messages (#882)

--- deployments/pulse3d/services/pulse3d/src/repository/notification_repository.py
+++ deployments/pulse3d/services/pulse3d/src/repository/notification_repository.py
@@ -1,4 +1,9 @@
-from models.models import NotificationResponse, SaveNotificationRequest, SaveNotificationResponse
+from models.models import (
+    NotificationResponse,
+    NotificationType,
+    SaveNotificationRequest,
+    SaveNotificationResponse,
+)
 import asyncpg
 
 
@@ -7,14 +12,34 @@ class NotificationRepository:
         self.pool = pool
 
     async def create(self, notification: SaveNotificationRequest) -> SaveNotificationResponse:
-        query = (
-            "INSERT INTO notifications (subject, body, notification_type) VALUES ($1, $2, $3) RETURNING id"
-        )
-
         async with self.pool.acquire() as con:
-            notification_id = await con.fetchval(
-                query, notification.subject, notification.body, notification.notification_type
-            )
+            async with con.transaction():
+                insert_notification_query = "INSERT INTO notifications (subject, body, notification_type) VALUES ($1, $2, $3) RETURNING id"
+
+                notification_id = await con.fetchval(
+                    insert_notification_query,
+                    notification.subject,
+                    notification.body,
+                    notification.notification_type,
+                )
+
+                select_customer_ids_query = "SELECT id FROM customers"
+                select_user_ids_query = "SELECT id FROM users"
+
+                match notification.notification_type:
+                    case NotificationType.CUSTOMERS_AND_USERS:
+                        audience_sub_query = f"({select_customer_ids_query} UNION {select_user_ids_query})"
+                    case NotificationType.CUSTOMERS:
+                        audience_sub_query = f"({select_customer_ids_query})"
+                    case NotificationType.USERS:
+                        audience_sub_query = f"({select_user_ids_query})"
+
+                insert_notification_messages_query = f"""
+                        INSERT INTO notification_messages (notification_id, recipient_id)
+                        SELECT '{notification_id}', id from {audience_sub_query} audience;
+                    """
+
+                await con.execute(insert_notification_messages_query)
 
         return SaveNotificationResponse(id=notification_id)
 
