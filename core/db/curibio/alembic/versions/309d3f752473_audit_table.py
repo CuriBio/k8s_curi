aa4bb44468ca9e295addda1724439ac4307bd3c9
Tanner Peterson <tannermp23@hotmail.com>
2024-11-12 15:28:54 -0600

    create DB migration file

---
+++ core/db/curibio/alembic/versions/309d3f752473_audit_table.py
@@ -0,0 +1,24 @@
+"""audit table
+
+Revision ID: 309d3f752473
+Revises: 6b689a41f489
+Create Date: 2024-11-12 13:01:19.060977
+
+"""
+
+# from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "309d3f752473"
+down_revision = "6b689a41f489"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
