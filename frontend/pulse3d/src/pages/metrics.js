29d8f56eb50d006af112c3ea97ed8cf47a6a6ff8
Tanner Peterson <tannermp23@hotmail.com>
2025-06-09 15:39:17 -0500

    update sidebar to show both metrics diagram versions

--- frontend/pulse3d/src/pages/metrics.js
+++ frontend/pulse3d/src/pages/metrics.js
@@ -1,7 +1,8 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import DashboardLayout from "@/components/layouts/DashboardLayout";
 import styled from "styled-components";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 // required for static export, default loader errors on build
 const imageLoader = ({ src }) => {
@@ -18,11 +19,13 @@ const BackgroundContainer = styled.div`
   justify-content: center;
 `;
 
-const getImages = (productPage) => {
+const getImages = (productPage, metricsVersion) => {
+  const version = metricsVersion || 1;
+
   if (productPage === "mantarray") {
     return (
       <Image
-        src={"/mantarray_twitch_metrics_diagram.png"}
+        src={`/mantarray_twitch_metrics_diagram_v${version}.png`}
         alt="Mantarray Twitch Metrics Diagram"
         width={800}
         height={1000}
@@ -34,7 +37,7 @@ const getImages = (productPage) => {
     return (
       <>
         <Image
-          src={"/nautilai_twitch_metrics_diagram.png"}
+          src={`/nautilai_twitch_metrics_diagram_v${version}.png`}
           alt="Nautilai Twitch Metrics Diagram"
           width={800}
           height={1000}
@@ -43,7 +46,7 @@ const getImages = (productPage) => {
         />
         <Image
           src={"/nautilai_detrending_and_normalization.png"}
-          alt="Nautilai Twitch Metrics Diagram"
+          alt="Nautilai Detrending and Normalization Diagram"
           width={800}
           height={1000}
           loader={imageLoader}
@@ -57,9 +60,15 @@ const getImages = (productPage) => {
 };
 
 export default function Metrics() {
+  const router = useRouter();
   const { productPage } = useContext(AuthContext);
+  const [metricsVersion, setMetricsVersion] = useState(null);
 
-  return <BackgroundContainer>{getImages(productPage)}</BackgroundContainer>;
+  useEffect(() => {
+    setMetricsVersion(router.query.id.toLowerCase() === "v2" ? 2 : 1);
+  }, [router.query]);
+
+  return <BackgroundContainer>{getImages(productPage, metricsVersion)}</BackgroundContainer>;
 }
 
 Metrics.getLayout = (page) => {
