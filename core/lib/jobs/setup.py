de0289c7c0a2bf13ef0ab460d6f4ada2f2738f71
Jason Person <jason.person@curibio.com>
2025-08-05 07:26:41 -0700

    bump patch version

--- core/lib/jobs/setup.py
+++ core/lib/jobs/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="jobs",
-    version="0.0.2",
+    version="0.0.3",
     description="CuriBio jobs queue",
     packages=find_packages(include=["jobs"]),
     install_requires=[
