ad7bb82e08336d4bf41e40f7d055ef20b24b34fb
Tanner Peterson <tannermp23@hotmail.com>
2025-11-14 10:15:21 -0600

    formatting of init_sealed_secrets

--- init_sealed_secrets.zsh
+++ init_sealed_secrets.zsh
@@ -29,83 +29,75 @@ if [[ -z "$2" ]]; then
 fi
 
 # advanced analysis deployment
-# kc create secret generic xxx -n advanced-analysis --dry-run=client \
-#     --from-literal=curibio_advanced_analysis=$ADVANCED_ANALYSIS_PASS \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-advanced-analysis-creds.yaml
-#
-# kc create secret generic xxx -n advanced-analysis --dry-run=client \
-#     --from-literal=curibio-email=$CURIBIO_EMAIL \
-#     --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-email-creds.yaml
-#
-# kc create secret generic xxx -n advanced-analysis --dry-run=client \
-#     --from-literal=jwt-secret=$JWT_SECRET \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-jwt-secret.yaml
+kc create secret generic xxx -n advanced-analysis --dry-run=client \
+    --from-literal=curibio_advanced_analysis=$ADVANCED_ANALYSIS_PASS \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-advanced-analysis-creds.yaml
+kc create secret generic xxx -n advanced-analysis --dry-run=client \
+    --from-literal=curibio-email=$CURIBIO_EMAIL \
+    --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-email-creds.yaml
+kc create secret generic xxx -n advanced-analysis --dry-run=client \
+    --from-literal=jwt-secret=$JWT_SECRET \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-jwt-secret.yaml
 
 # apiv2 deployment
-# kc create secret generic xxx -n apiv2 --dry-run=client \
-#     --from-literal=curibio-email=$CURIBIO_EMAIL \
-#     --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-email-creds.yaml
-#
-# kc create secret generic xxx -n apiv2 --dry-run=client \
-#     --from-literal=curibio_event_broker=$EVENT_BROKER_PASS \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-event-broker-creds.yaml
-#
-# kc create secret generic xxx -n apiv2 --dry-run=client \
-#     --from-literal=jwt-secret=$JWT_SECRET \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-jwt-secret.yaml
-#
-# kc create secret generic xxx -n apiv2 --dry-run=client \
-#     --from-literal=curibio_mantarray=$MANTARRAY_USER_PASS \
-#     --from-literal=curibio_mantarray_ro=$MANTARRAY_USER_PASS_RO\
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-mantarray-creds.yaml
-#
-# kc create secret generic xxx -n apiv2 --dry-run=client \
-#     --from-literal=curibio_users=$TABLE_USER_PASS \
-#     --from-literal=curibio_users_ro=$TABLE_USER_PASS_RO \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-users-creds.yaml
+kc create secret generic xxx -n apiv2 --dry-run=client \
+    --from-literal=curibio-email=$CURIBIO_EMAIL \
+    --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-email-creds.yaml
+kc create secret generic xxx -n apiv2 --dry-run=client \
+    --from-literal=curibio_event_broker=$EVENT_BROKER_PASS \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-event-broker-creds.yaml
+kc create secret generic xxx -n apiv2 --dry-run=client \
+    --from-literal=jwt-secret=$JWT_SECRET \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-jwt-secret.yaml
+kc create secret generic xxx -n apiv2 --dry-run=client \
+    --from-literal=curibio_mantarray=$MANTARRAY_USER_PASS \
+    --from-literal=curibio_mantarray_ro=$MANTARRAY_USER_PASS_RO\
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-mantarray-creds.yaml
+kc create secret generic xxx -n apiv2 --dry-run=client \
+    --from-literal=curibio_users=$TABLE_USER_PASS \
+    --from-literal=curibio_users_ro=$TABLE_USER_PASS_RO \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-users-creds.yaml
 
 # pulse3d deployment
-# kc create secret generic xxx -n pulse3d --dry-run=client \
-#     --from-literal=curibio-email=$CURIBIO_EMAIL \
-#     --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-email-creds.yaml
-#
-# kc create secret generic xxx -n pulse3d --dry-run=client \
-#     --from-literal=curibio_jobs=$JOBS_USER_PASS \
-#     --from-literal=curibio_jobs_ro=$JOBS_USER_PASS_RO \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-jobs-creds.yaml
-#
-# kc create secret generic xxx -n pulse3d --dry-run=client \
-#     --from-literal=jwt-secret=$JWT_SECRET \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-jwt-secret.yaml
+kc create secret generic xxx -n pulse3d --dry-run=client \
+    --from-literal=curibio-email=$CURIBIO_EMAIL \
+    --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-email-creds.yaml
+kc create secret generic xxx -n pulse3d --dry-run=client \
+    --from-literal=curibio_jobs=$JOBS_USER_PASS \
+    --from-literal=curibio_jobs_ro=$JOBS_USER_PASS_RO \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-jobs-creds.yaml
+kc create secret generic xxx -n pulse3d --dry-run=client \
+    --from-literal=jwt-secret=$JWT_SECRET \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-jwt-secret.yaml
 
 # advanced analysis jobs
-# kc create secret generic xxx -n advanced-analysis --dry-run=client \
-#     --from-literal=advanced_analysis_queue_processor_ro=$ADVANCED_ANALYSIS_QUEUE_PROCESSOR_RO_PASS \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./jobs/advanced-analysis/manifests/overlays/$1/advanced-analysis-queue-processor-creds.yaml
+kc create secret generic xxx -n advanced-analysis --dry-run=client \
+    --from-literal=advanced_analysis_queue_processor_ro=$ADVANCED_ANALYSIS_QUEUE_PROCESSOR_RO_PASS \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./jobs/advanced-analysis/manifests/overlays/$1/advanced-analysis-queue-processor-creds.yaml
 
 # pulse3d jobs
-# kc create secret generic xxx -n pulse3d --dry-run=client \
-#     --from-literal=pulse3d_queue_processor_ro=$PULSE3D_QUEUE_PROCESSOR_RO_PASS \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./jobs/pulse3d/manifests/overlays/$1/pulse3d-queue-processor-creds.yaml
+kc create secret generic xxx -n pulse3d --dry-run=client \
+    --from-literal=pulse3d_queue_processor_ro=$PULSE3D_QUEUE_PROCESSOR_RO_PASS \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./jobs/pulse3d/manifests/overlays/$1/pulse3d-queue-processor-creds.yaml
 
 # grafana
-# kc create secret generic xxx -n argocd --dry-run=client \
-#     --from-literal=grafana_pass_ro=$GRAFANA_PASS_RO \
-#     -o yaml \
-#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./cluster/manifests/grafana/overlays/$1/grafana-creds.yaml
+kc create secret generic xxx -n argocd --dry-run=client \
+    --from-literal=grafana_pass_ro=$GRAFANA_PASS_RO \
+    -o yaml \
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./cluster/manifests/grafana/overlays/$1/grafana-creds.yaml
