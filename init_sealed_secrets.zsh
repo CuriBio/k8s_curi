481f88ff0354b0a6e9626abfc40b5afd406c4f95
Tanner Peterson <tannermp23@hotmail.com>
2025-11-13 18:11:34 -0600

    update p3d deployment secrets

--- init_sealed_secrets.zsh
+++ init_sealed_secrets.zsh
@@ -46,30 +46,48 @@ fi
 #     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/advanced-analysis/manifests/overlays/$1/curibio-jwt-secret.yaml
 
 # apiv2 deployment
-kc create secret generic xxx -n apiv2 --dry-run=client \
+# kc create secret generic xxx -n apiv2 --dry-run=client \
+#     --from-literal=curibio-email=$CURIBIO_EMAIL \
+#     --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
+#     -o yaml \
+#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-email-creds.yaml
+#
+# kc create secret generic xxx -n apiv2 --dry-run=client \
+#     --from-literal=curibio_event_broker=$EVENT_BROKER_PASS \
+#     -o yaml \
+#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-event-broker-creds.yaml
+#
+# kc create secret generic xxx -n apiv2 --dry-run=client \
+#     --from-literal=jwt-secret=$JWT_SECRET \
+#     -o yaml \
+#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-jwt-secret.yaml
+#
+# kc create secret generic xxx -n apiv2 --dry-run=client \
+#     --from-literal=curibio_mantarray=$MANTARRAY_USER_PASS \
+#     --from-literal=curibio_mantarray_ro=$MANTARRAY_USER_PASS_RO\
+#     -o yaml \
+#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-mantarray-creds.yaml
+#
+# kc create secret generic xxx -n apiv2 --dry-run=client \
+#     --from-literal=curibio_users=$TABLE_USER_PASS \
+#     --from-literal=curibio_users_ro=$TABLE_USER_PASS_RO \
+#     -o yaml \
+#     | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-users-creds.yaml
+
+# pulse3d deployment
+kc create secret generic xxx -n pulse3d --dry-run=client \
     --from-literal=curibio-email=$CURIBIO_EMAIL \
     --from-literal=curibio-email-password=$CURIBIO_EMAIL_PASSWORD \
     -o yaml \
-    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-email-creds.yaml
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-email-creds.yaml
 
-kc create secret generic xxx -n apiv2 --dry-run=client \
-    --from-literal=curibio_event_broker=$EVENT_BROKER_PASS \
+kc create secret generic xxx -n pulse3d --dry-run=client \
+    --from-literal=curibio_jobs=$JOBS_USER_PASS \
+    --from-literal=curibio_jobs_ro=$JOBS_USER_PASS_RO \
     -o yaml \
-    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-event-broker-creds.yaml
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-jobs-creds.yaml
 
-kc create secret generic xxx -n apiv2 --dry-run=client \
+kc create secret generic xxx -n pulse3d --dry-run=client \
     --from-literal=jwt-secret=$JWT_SECRET \
     -o yaml \
-    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-jwt-secret.yaml
-
-kc create secret generic xxx -n apiv2 --dry-run=client \
-    --from-literal=curibio_mantarray=$MANTARRAY_USER_PASS \
-    --from-literal=curibio_mantarray_ro=$MANTARRAY_USER_PASS_RO\
-    -o yaml \
-    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-mantarray-creds.yaml
-
-kc create secret generic xxx -n apiv2 --dry-run=client \
-    --from-literal=curibio_users=$TABLE_USER_PASS \
-    --from-literal=curibio_users_ro=$TABLE_USER_PASS_RO \
-    -o yaml \
-    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/apiv2/manifests/overlays/$1/curibio-users-creds.yaml
+    | kubeseal --kubeconfig=$3 --format yaml --merge-into ./deployments/pulse3d/manifests/overlays/$1/curibio-jwt-secret.yaml
