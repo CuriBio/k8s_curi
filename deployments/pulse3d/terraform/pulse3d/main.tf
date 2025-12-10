eebd9f1e5dff92b3a9d466463c1fd7578f454511
Tanner Peterson <tannermp23@hotmail.com>
2025-12-10 16:12:48 -0600

    create private downloads bucket

--- deployments/pulse3d/terraform/pulse3d/main.tf
+++ deployments/pulse3d/terraform/pulse3d/main.tf
@@ -1,3 +1,11 @@
+locals {
+  allowed_origins = {
+    test = ["https://dashboard.curibio-test.com", "http://localhost:3000"]
+    modl = ["https://dashboard.curibio-modl.com"]
+    prod = ["https://dashboard.curibio.com"]
+  }
+}
+
 resource "aws_ecr_repository" "pulse3d_ecr_repo" {
   name                 = "pulse3d_api"
   image_tag_mutability = "IMMUTABLE"
@@ -102,7 +110,73 @@ resource "aws_s3_bucket_cors_configuration" "pulse3d_uploads_bucket" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT"]
-    allowed_origins = var.cluster_name == "prod" ? ["https://dashboard.curibio.com"] : ["https://dashboard.curibio-test.com", "https://dashboard.curibio-modl.com", "http://localhost:3000"]
+    allowed_origins = local.allowed_origins[var.cluster_name]
     expose_headers = ["ETag"]
+    max_age_seconds = 0
+  }
+}
+
+
+resource "aws_s3_bucket" "private_downloads_bucket" {
+  bucket = "curi-${var.cluster_name}-private-downloads"
+}
+
+resource "aws_s3_bucket_policy" "private_downloads_bucket" {
+  bucket = aws_s3_bucket.private_downloads_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnforceTls"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "${aws_s3_bucket.private_downloads_bucket.arn}/*",
+          "${aws_s3_bucket.private_downloads_bucket.arn}",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_ownership_controls" "private_downloads_bucket" {
+  bucket = aws_s3_bucket.private_downloads_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "private_downloads_bucket" {
+  bucket = aws_s3_bucket.private_downloads_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "private_downloads_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.private_downloads_bucket]
+
+  bucket = aws_s3_bucket.private_downloads_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_cors_configuration" "private_downloads_bucket" {
+  bucket = aws_s3_bucket.private_downloads_bucket.bucket
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = local.allowed_origins[var.cluster_name]
+    expose_headers = ["ETag"]
+    max_age_seconds = 0
   }
 }
